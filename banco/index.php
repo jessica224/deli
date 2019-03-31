7366b7202ff327b9437da5c97c63bf422c463b85
jessica224 <44957861+jessica224@users.noreply.github.com>
2019-03-31 20:38:41 -0300

    Update index.php

--- banco/index.php
+++ banco/index.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("172.31.52.179", "userXVS", "ygrvIstqnaHy7glq", "codigofonte");
+$link = mysqli_connect("10.131.57.104", "userXVS", "ygrvIstqnaHy7glq", "codigofonte");
  
 if (!$link) {
     echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
