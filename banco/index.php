c45db73aea72b1861661bc69f6358b4ecf7ea06e
jessica224 <44957861+jessica224@users.noreply.github.com>
2019-03-31 20:45:40 -0300

    Update index.php

--- banco/index.php
+++ banco/index.php
@@ -2,7 +2,7 @@
 $link = mysqli_connect("ip-172-31-52-179.us-west-2.compute.internal", "userXVS", "ygrvIstqnaHy7glq", "codigofonte");
  
 if (!$link) {
-    echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
+    echo "Error: Falhaa ao conectar-se com o banco de dados MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
